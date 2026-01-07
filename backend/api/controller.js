14ead19aef24657d4adf2552812ab377b0a2bb8e
Justin Klava <justin.klava@esgworks.com>
2026-01-07 14:28:41 -0500

    added and tested functions for finding possible words

--- backend/api/controller.js
+++ backend/api/controller.js
@@ -2,8 +2,48 @@ import fs from 'fs';
 import path from 'path';
 import {fileURLToPath} from 'url';
 
-export { };
+export { findWords };
 
 const currentDirectory = path.dirname(fileURLToPath(import.meta.url));
 
 const dictionary = fs.readFileSync(path.resolve(currentDirectory, 'data', 'shuffled_real_wordles.txt'), 'utf8').split('\n');
+
+const findWords = (greenLetters, yellowLetters, greyLetters) => {
+    const possibleWords = [];
+    //possibleWords.push('abcde', 'fghij', 'klmno');
+    for(const word of dictionary) {
+        if(word === '' || word == null) continue;
+        if(hasGreys(word, greyLetters)) continue;
+        if(!matchesGreens(word, greenLetters)) continue;
+        if(!matchesYellows(word, yellowLetters)) continue;
+        possibleWords.push(word);
+    }
+    return possibleWords;
+};
+
+const hasGreys = (word, letters) => {
+    for(const letter of letters.toUpperCase()) {
+        if (word.toUpperCase().includes(letter))
+            return true;
+    }
+    return false;
+};
+
+const matchesGreens = (word, letters) => {
+    for (let i = 0; i < 5; i++) {
+        if (letters[i] === '') continue;
+        if (word[i].toUpperCase() !== letters[i].toUpperCase()) return false;
+    }
+    return true;
+};
+
+const matchesYellows = (word, letters) => {
+    for (let i = 0; i < 5; i++) {
+        for (const letter of letters[i]) {
+            if (letter === '' || letter == null) continue;
+            if (!word.toUpperCase().includes(letter.toUpperCase())) return false;
+            if (word[i].toUpperCase() === letter.toUpperCase()) return false;
+        }
+    }
+    return true;
+};
