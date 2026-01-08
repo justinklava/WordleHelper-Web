2f63cbf4eebb5b56330cd83e7e8c2b84ef586e0b
Justin Klava <email@justinklava.com>
2026-01-08 00:21:55 -0500

    completed route functionality

--- backend/api/router.js
+++ backend/api/router.js
@@ -1,5 +1,22 @@
 import express from 'express';
-import { } from './controller.js';
+import { findWords } from './controller.js';
 
 const api = express.Router();
 export default api;
+
+api.post('/PossibleWords', (req, res, next) => {
+    const testResponse = {
+        possibleWords: ['abcde', 'fghij', 'klmno', 'pqrst', 'uvwxy', 'zzzzz'],
+        yellowLetters: [ 'asdfg', 'asd', 'jk', 'lj', '' ],
+        greenLetters: [ '', '', 'i', '', ''],
+        greyLetters: 'jkluio'
+    };
+    const { yellowLetters, greenLetters, greyLetters } = req.body;
+    const possibleWords = findWords(greenLetters, yellowLetters, greyLetters);
+    res.send(JSON.stringify({
+        possibleWords: possibleWords,
+        greenLetters: greenLetters,
+        yellowLetters: yellowLetters,
+        greyLetters: greyLetters
+    }));
+});
